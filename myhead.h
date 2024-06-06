fd190a8caadb93ec11d739eaeefc0d7bf0e8f65a
孫詠鈞 <jimmysun609@gmail.com>
2024-06-07 02:19:36 +0800

    myhead.h

--- myhead.h
+++ myhead.h
@@ -11,6 +11,17 @@ typedef struct {
 
 } Student;
 
-void printStudents (Student students[], int numStudents);
+void randominitStudents (Student students[], int numStudents);
+
+//void sortStudentsByname(Student* students, int numStudents);
+//void sortStudentsBydepartment(Student* students, int numStudents);
+//void sortStudentsBygrade(Student* students, int numStudents);
+//void sortStudentsBySID(Student* students, int numStudents);
+void sortStudents(Student* students, int numStudents, const char* sortBy);
+
+void printStudentsByname(Student students[], int numStudents);
+void printStudentsBydepartment(Student students[], int numStudents);
+void printStudentsBygrade(Student students[], int numStudents);
+void printStudentsBySID(Student students[], int numStudents);
 
 #endif // MYHEAD_H
